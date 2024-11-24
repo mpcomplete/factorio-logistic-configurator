702fcd415a822c761ae026e59ca079e05ce80fed
Matt Perry <mpcomplete@gmail.com>
2024-11-23 21:40:26 -0500

    description

--- zip.sh
+++ zip.sh
@@ -3,6 +3,7 @@ name=LogisticConfigurator_1.0.1
 mkdir $name
 cp -a * $name
 rm $name/*.sh $name/*.zip $name/$name $name/*.png -rf
+cp thumbnail.png $name
 rm -rf ${name}.zip
 7z a ${name}.zip $name
 rm -rf $name
